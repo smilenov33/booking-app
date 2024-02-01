48d0b0bc905a699a4f321ec2139fbeb0d870f68a
sasheto_33 <sashomilenov2@gmail.com>
2024-02-01 22:26:41 +0200

    add cloudinary

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,13 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
